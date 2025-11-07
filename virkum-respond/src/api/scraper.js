97e3a8bbbd13d4569d48470208fffbea6d04fcb6
Ólafur <haraldsson48@gmail.com>
2025-11-07 16:56:13 +0000

    frontend nær að setja inn í cloud

--- virkum-respond/src/api/scraper.js
+++ virkum-respond/src/api/scraper.js
@@ -2,8 +2,5 @@ import { apiFetch } from "./client";
 
 export async function scrapeWebsite(url) {
   if (!url) throw new Error("Missing URL");
-
-  const res = await apiFetch(`/scrape?url=${encodeURIComponent(url)}`);
-
-  return res;
+  return apiFetch(`/scrape?url=${encodeURIComponent(url)}`);
 }
